fffadeb97f4cc90d5e71917443bc9e7061ccd6bb
Austin <austinm9506@gmail.com>
2022-06-07 01:03:52 -0400

    Added an endpoint for deleting chat entries.

--- controller/chat_data.js
+++ controller/chat_data.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllChats, getAChat, createChat, editChat } from "../model/chat_data.js"
+import { getAllChats, getAChat, createChat, editChat, deleteChat } from "../model/chat_data.js"
 const chatRouter = express.Router()
 
 chatRouter.get('/api', async (req, res) => {
@@ -33,6 +33,15 @@ chatRouter.put("/api/:id", async (req, res) => {
     try {
         const editedChat = await editChat(req.params.id, req.body)
         res.json(editedChat)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+chatRouter.delete("/api/:id", async (req, res) => {
+    try {
+        const deletedChat = await deleteChat(req.params.id)
+        res.sendStatus(200)
     } catch (error) {
         console.error(error)
     }
